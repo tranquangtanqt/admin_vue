4968693e97e84d48b2a58fb84e09bebad7001168
Tran Quang Tan <tantq@aureole.local>
2019-10-21 17:31:27 +0700

    basic interface

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,13 @@ import store from "./store";
 
 Vue.config.productionTip = false;
 
+import './assets/resources/themes/nova-light/theme.css';
+import './assets/resources/primevue.min.css';
+import './assets/resources/primeflex/primeflex.css';
+import './assets/resources/primeicons/primeicons.css';
+import './assets/layout/layout.scss';
+
+Vue.config.productionTip = false;
 new Vue({
   router,
   store,
